1a6d83b701d183951ec7b872a32ab6f5b3a24ca0
Denys Fedoryshchenko <denys.f@collabora.com>
2025-05-20 14:26:06 +0300

    Updates for self-hosted kcidb
    Signed-off-by: Denys Fedoryshchenko <denys.f@collabora.com>

---
+++ scripts/dbinit.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo "Initializing database..."
+psql -h db -U postgres -c "CREATE ROLE kcidb WITH LOGIN PASSWORD 'kcidb';"
+psql -h db -U postgres -c "CREATE DATABASE kcidb WITH OWNER kcidb;"
+psql -h db -U postgres -c "CREATE ROLE kcidb_editor WITH LOGIN PASSWORD 'kcidb';"
+psql -h db -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE kcidb TO kcidb_editor;"
+psql -h db -U postgres -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kcidb_editor;"
+psql -h db -U postgres -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO kcidb_editor;"
+psql -h db -U postgres -c "GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO kcidb_editor;"
+# add kcidb_editor who can write
+psql -h db -U postgres -c "ALTER ROLE kcidb_editor SET client_encoding TO 'utf8';"
+psql -h db -U postgres -c "ALTER ROLE kcidb_editor SET default_transaction_isolation TO 'read committed';"
+psql -h db -U postgres -c "ALTER ROLE kcidb_editor SET timezone TO 'UTC';"
+# add kcidb_viewer who can read only
+psql -h db -U postgres -c "CREATE ROLE kcidb_viewer WITH LOGIN PASSWORD 'kcidb';"
+psql -h db -U postgres -c "GRANT CONNECT ON DATABASE kcidb TO kcidb_viewer;"
+psql -h db -U postgres -c "GRANT USAGE ON SCHEMA public TO kcidb_viewer;"
+psql -h db -U postgres -c "GRANT SELECT ON ALL TABLES IN SCHEMA public TO kcidb_viewer;"
+psql -h db -U postgres -c "GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO kcidb_viewer;"
+psql -h db -U postgres -c "ALTER ROLE kcidb_viewer SET client_encoding TO 'utf8';"
+psql -h db -U postgres -c "ALTER ROLE kcidb_viewer SET default_transaction_isolation TO 'read committed';"
+psql -h db -U postgres -c "ALTER ROLE kcidb_viewer SET timezone TO 'UTC';"
+echo "Database initialized."
+echo "Creating DB schema for kcidb..."
+kcidb-db-init -d "${PG_URI}" --ignore-initialized
+echo "DB schema created."
---
+++ self-hosted.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -f .env ]; then
+  echo ".env file not found!"
+  exit 1
+fi
+# check if JWT_SECRET=your_jwt_secret is default values, then generate new using openssl
+if grep -q "JWT_SECRET=your_jwt_secret" .env; then
+  echo "Generating new JWT_SECRET..."
+  JWT_SECRET=$(openssl rand -hex 32)
+  sed -i "s/JWT_SECRET=your_jwt_secret/JWT_SECRET=$JWT_SECRET/" .env
+  echo "New JWT_SECRET generated: $JWT_SECRET"
+else
+  echo "JWT_SECRET already set in .env"
+fi
+
+sudo docker compose --profile=self-hosted up -d --build
